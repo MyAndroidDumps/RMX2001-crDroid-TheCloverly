ff1a9e511dbab0e00f2bcc2c9e19cd0824d726c0
SriBalaji <sribalajisenthilkumar@gmail.com>
2021-05-10 10:16:05 +0530

    Update sync_rom.sh

--- sync_rom.sh
+++ sync_rom.sh
@@ -6,3 +6,12 @@ set -exv
 repo init -u git://github.com/crdroidandroid/android.git -b 11.0
 git clone https://github.com/Realme-G90T-Series/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# patches
+cd external/selinux 
+curl -L http://ix.io/2FhM > sasta.patch 
+git am sasta.patch 
+cd /tmp/rom
+cd frameworks/av 
+wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch 
+patch -p1 < *.patch
