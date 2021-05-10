8132830cf6615a457dd965099b10e3fe7eda274a
SriBalaji <sribalajisenthilkumar@gmail.com>
2021-05-10 09:55:55 +0530

    Update sync_rom.sh

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,7 +3,6 @@
 set -exv
 
 # sync rom
-repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1
-git clone https://github.com/Realme-G70-Series/local_manifest.git -b 11-staging --depth=1 .repo/local_manifests
+repo init -u git://github.com/crdroidandroid/android.git -b 11.0
+git clone https://github.com/Realme-G90T-Series/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-
